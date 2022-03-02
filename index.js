ca2f54ab948fcdd1e3c9573011c3429fef2666c7
Hitesh Kumar <hiteshkumaragra00@gmail.com>
2022-03-02 11:53:52 +0530

    Add Feature/MVC-initial

--- index.js
+++ index.js
@@ -1,30 +1,100 @@
 
-const tag_id = document.getElementById("recommendation");
+// const tag_id = document.getElementById("recommendation");
 
-fetch("./index.json").then(response =>{
-    return response.json();
-}).then(function (data){
-    for(let x of data['foodData']){
-        tag_id.innerHTML+=
-        `<div class="firstDish">
-        <div id="firstDishandname">
-            ${x.dishItemName}
-            <img src="first.jpeg" /> 
-        </div>
+// fetch("./index.json").then(response => {
+//     return response.json();
+// }).then(function (data) {
+//     for (let x of data['foodData']) {
+//         tag_id.innerHTML +=
+//             `<div class="firstDish">
+//         ${x.dishItemName}
+//         <div id="firstDishandname_img" > 
+//             <img src="${x.image}" width="100" /> 
+//             <button onclick="addToCartClicked()">Add To Cart</button>
+//         </div>
 
-        <div class="pricerecom">
-        ${x.dishPrice}
-        </div>
+//         <div class="pricerecom">
+//         ${x.dishPrice}
+//         </div>
         
-        <div id="firstDishContent">
-        ${x.aboutdescripton}
-        </div>
-        <br> </br>
-        <hr class="descrecom">
-    </div>
-    `
-        // console.log(x.id + " "+ x.dishPrice);
-    }
-})
-// import foodData from "./index.json" assert { type: "json" };
+//         <div id="firstDishContent">
+//         ${x.aboutdescripton}
+//         </div>
+//         <br> </br>
+//         <hr class="descrecom">
+//     </div>
+//     `
+//     }
+// })
+// // import foodData from "./index.json" assert { type: "json" };
 
+// {/* <button onclick="myFunction()">Add to Cart</button> */}
+
+import foodData from "./index.json" assert { type: "json" };
+
+let model={
+    itemsData:foodData,
+    init:function(passeddata){
+        this.itemsData=passeddata;
+    }
+    setData:function(itemsData){
+        return this.itemsData;
+    }
+};
+
+let view={
+    init: function() {
+        this.render();
+    },
+
+    itemsListView:function(){
+         fetch("./index.json").then(response => {
+             response.json();
+        }).then(function (data) {
+            for (let x of data['foodData']) {
+                tag_id.innerHTML +=
+                    `<div class="firstDish">
+                ${x.dishItemName}
+                <div id="firstDishandname_img" > 
+                    <img src="${x.image}" width="100" /> 
+                    <button onclick="addToCartClicked()">Add To Cart</button>
+                </div>
+        
+                <div class="pricerecom">
+                ${x.dishPrice}
+                </div>
+                
+                <div id="firstDishContent">
+                ${x.aboutdescripton}
+                </div>
+                <br> </br>
+                <hr class="descrecom">
+            </div>
+            `
+            }
+        })
+    },
+
+    render:function(){
+        this.itemsListView();
+    }
+};
+
+let controller={
+    init:function(){
+        model.init(foodData);
+        view.render();
+    },
+    getItem:function(){
+        return model.itemsData;
+    }
+}
+
+
+// fetch('./index.json')
+//     .then((response) => response.json())
+//     .then((item) => {
+//         profileController.init(item.profileData);
+//         postsController.init(item);
+//         footerController.init();
+//     });
